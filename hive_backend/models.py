25d8bf901ddfbd32fdcc4b6114ae6d8fa625af78
JoonasS94 <sariola94@gmail.com>
2024-11-20 18:42:31 +0200

    Posting works and using same hashtag also.

--- hive_backend/models.py
+++ hive_backend/models.py
@@ -13,7 +13,7 @@ class CustomUser(AbstractUser):
 CustomUser = get_user_model()  # This retrieves the custom user model
 
 class Hashtag(models.Model):
-    name = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=20, unique=False)
 
     def __str__(self):
         return self.name
