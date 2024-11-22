f521c49d2e4290bea5a3864a810d9a0700960fdc
Joonas Sariola <91365380+JoonasS94@users.noreply.github.com>
2024-11-22 09:42:41 +0200

    Prevent date__joined being present in PUT and PATCH endpoints
    date__joined is not visible anymore in PUT and PATCH /users/ endpoints, put still present in GET /users/ .

--- hive_backend/serializers.py
+++ hive_backend/serializers.py
@@ -45,7 +45,6 @@ class HashtagSerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 
-# User Serializer
 class UserSerializer(serializers.ModelSerializer):
     amount_of_liked_users = serializers.SerializerMethodField()  # Number of users this user has liked
     liked_user_id = serializers.SerializerMethodField()  # IDs of users this user has liked
@@ -54,7 +53,6 @@ class UserSerializer(serializers.ModelSerializer):
     id_and_name_of_followed_hashtags = serializers.SerializerMethodField()  # ID and name of followed hashtags
     posts_count = serializers.SerializerMethodField()  # Count of posts created by the user
     liked_posts_count = serializers.SerializerMethodField()  # Count of liked posts by the user
-    date_joined = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')  # Mukautettu päivämäärän ja ajan formaatti
 
     class Meta:
         model = User
@@ -62,8 +60,10 @@ class UserSerializer(serializers.ModelSerializer):
             'id', 'email', 'username', 'bio',
             'amount_of_liked_users', 'liked_user_id',
             'amount_of_me_liked_users', 'amount_of_followed_hashtags',
-            'id_and_name_of_followed_hashtags', 'posts_count', 'liked_posts_count','date_joined',
+            'id_and_name_of_followed_hashtags', 'posts_count', 'liked_posts_count',
+            'date_joined',  # Included for GET requests
         ]
+        read_only_fields = ['date_joined']  # Make date_joined read-only for updates
 
     def get_amount_of_liked_users(self, obj):
         return LikedUsers.objects.filter(liker=obj).count()
@@ -87,6 +87,20 @@ class UserSerializer(serializers.ModelSerializer):
 
     def get_liked_posts_count(self, obj):
         return LikedPosts.objects.filter(user=obj).count()
+
+    def to_internal_value(self, data):
+        # Explicitly remove 'date_joined' from the incoming data for PUT/PATCH requests
+        if 'date_joined' in data:
+            del data['date_joined']
+        return super().to_internal_value(data)
+
+    def to_representation(self, instance):
+        # Customize the representation for GET requests to include date_joined
+        representation = super().to_representation(instance)
+        representation['date_joined'] = instance.date_joined.strftime('%Y-%m-%d %H:%M:%S')  # Customize the format if needed
+        return representation
+
+
 
 
 # Post Serializer
